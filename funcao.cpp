836bab90679bb8ba03bec9d5bd84f799f07b751c
thiagomarinh <thiagoo_100@hotmail.com>
2021-11-17 23:20:08 -0300

    Update Função.cpp
    Adicionada a função de calculo de temperatura

--- funcao.cpp
+++ funcao.cpp
@@ -1,18 +1,32 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
 int media( int x, int y);
+int converteTemperatura(int x);
+
 int main(){
-    int a = 10, b = 20;
-    cout << (media(a,b));
+    int a = 10, b = 20, c = 19;
     
+    cout << ("A media é \n");
+    cout << (media(a,b));
+    cout << "\n";
+    cout << "A temperatura em Gruas F é \n";
+    cout << (converteTemperatura(c));
 
 }
 
+//Calcula a média entre dois numeros
 int media(int x, int y){
     int resultado;
     resultado = (x+y)/2;
     return resultado;
+}
+
+//converte temperatura de Celsius para Fahrenheit
+int converteTemperatura(int x){
+    int converte;
+    converte = (x * 9/5) + 32;
+    return converte;
 }
 
 
